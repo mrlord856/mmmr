418d1b12e6abf6dafbfb486ebb81d53ca20406fb
mrlord856 <151881197+mrlord856@users.noreply.github.com>
2024-02-14 10:34:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -81,21 +81,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/feeef194eac477862bff6.jpg"
+    "START_IMG_URL", "https://graph.org/file/5d3b2dde4ef0da9baf213.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/feeef194eac477862bff6.jpg"
+    "PING_IMG_URL", "https://graph.org/file/5d3b2dde4ef0da9baf213.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/feeef194eac477862bff6.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/feeef194eac477862bff6.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/feeef194eac477862bff6.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/feeef194eac477862bff6.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/feeef194eac477862bff6.jpg"
-SOUNCLOUD_IMG_URL = "https://telegra.ph/file/feeef194eac477862bff6.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/feeef194eac477862bff6.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/feeef194eac477862bff6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/feeef194eac477862bff6.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/feeef194eac477862bff6.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/5d3b2dde4ef0da9baf213.jpg"
+STATS_IMG_URL = "https://graph.org/file/5d3b2dde4ef0da9baf213.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/5d3b2dde4ef0da9baf213.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/5d3b2dde4ef0da9baf213.jpg"
+STREAM_IMG_URL = "https://graph.org/file/5d3b2dde4ef0da9baf213.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/5d3b2dde4ef0da9baf213.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/5d3b2dde4ef0da9baf213.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/5d3b2dde4ef0da9baf213.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/5d3b2dde4ef0da9baf213.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/5d3b2dde4ef0da9baf213.jpg"
 
 
 def time_to_seconds(time):
